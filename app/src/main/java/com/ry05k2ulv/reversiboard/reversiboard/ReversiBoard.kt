11eaae88eff2fc0871e21c4c191bfc61eda154c4
ry05k2ulv <ryosuke.10117@gmail.com>
2024-01-13 15:14:05 +0900

    fix: Fix testcase of ReversiBoard

--- app/src/main/java/com/ry05k2ulv/reversiboard/reversiboard/ReversiBoard.kt
+++ app/src/main/java/com/ry05k2ulv/reversiboard/reversiboard/ReversiBoard.kt
@@ -4,26 +4,36 @@ import com.ry05k2ulv.reversiboard.reversiboard.Piece.*
 import java.util.ArrayDeque
 import java.util.Stack
 
-typealias PieceList = List<Piece>
+const val boardWidth = 8
+const val boardArea = boardWidth * boardWidth
+
+internal val dx = arrayOf(0, 1, 1, 1, 0, -1, -1, -1)
+internal val dy = arrayOf(1, 1, 0, -1, -1, -1, 0, 1)
+
+operator fun List<Piece>.get(x: Int, y: Int): Piece {
+    return this[x + y * boardWidth]
+}
+
+operator fun MutableList<Piece>.set(x: Int, y: Int, cell: Piece) {
+    this[x + y * boardWidth] = cell
+}
 
 class ReversiBoard(
-    val width: Int = 8,
     val undoLimit: Int = 1024,
 ) {
-    private val dx = arrayOf(0, 1, 1, 1, 0, -1, -1, -1)
-    private val dy = arrayOf(1, 1, 0, -1, -1, -1, 0, 1)
 
-    var elements: PieceList = List(width * width) { Empty }
+
+    var boardData = BoardData()
         private set
-    private val undoDeque = ArrayDeque<PieceList>()
-    private val redoStack = Stack<PieceList>()
+    private val undoDeque = ArrayDeque<BoardData>()
+    private val redoStack = Stack<BoardData>()
 
-    operator fun get(x: Int, y: Int) = elements[x, y]
+    operator fun get(x: Int, y: Int) = boardData.elements[x, y]
 
-    fun updateBoard(newBoard: PieceList) {
-        require(newBoard.size == width * width) { "Invalid length" }
-        undoDeque.addLast(elements)
-        elements = newBoard
+    fun updateBoard(newBoard: BoardData) {
+        require(newBoard.elements.size == boardArea) { "Invalid length" }
+        undoDeque.addLast(boardData)
+        boardData = newBoard
         redoStack.clear()
         if (undoDeque.size > undoLimit)
             undoDeque.removeFirst()
@@ -32,18 +42,19 @@ class ReversiBoard(
     fun canUndo() = !undoDeque.isEmpty()
     fun canRedo() = !redoStack.isEmpty()
     fun undo() {
-        redoStack.push(elements)
-        elements = undoDeque.removeLast()
+        redoStack.push(boardData)
+        boardData = undoDeque.removeLast()
     }
+
     fun redo() {
-        undoDeque.addLast(elements)
-        elements = redoStack.pop()
+        undoDeque.addLast(boardData)
+        boardData = redoStack.pop()
     }
 
     fun drop(piece: Piece, x: Int, y: Int, overwrite: Boolean = false, reversible: Boolean = true) {
-        val nextBoard = elements.toMutableList()
-        if (nextBoard[x, y] != Empty && !overwrite) return
-        nextBoard[x, y] = piece
+        val elements = boardData.elements.toMutableList()
+        if (elements[x, y] != Empty && !overwrite) return
+        elements[x, y] = piece
         if (!reversible) return
         /* nextBoard[x, y] == Empty && reversible */
         repeat(8) { i ->
@@ -52,48 +63,31 @@ class ReversiBoard(
                 val nextX = x + dx[i] * j
                 val nextY = y + dy[i] * j
                 if (
-                    nextX !in 0 until width ||
-                    nextY !in 0 until width ||
-                    nextBoard[nextX, nextY] == Empty
+                    nextX !in 0 until boardWidth ||
+                    nextY !in 0 until boardWidth ||
+                    elements[nextX, nextY].isEmpty()
                 ) return@repeat
-                if (nextBoard[nextX, nextY] == piece)
+                if (elements[nextX, nextY] == piece)
                     break
                 j++
             }
             while (--j > 0)
-                nextBoard[x + dx[i] * j, y + dy[i] * j] = piece
+                elements[x + dx[i] * j, y + dy[i] * j] = piece
         }
-        updateBoard(nextBoard)
+        updateBoard(BoardData(elements))
     }
 
     fun reset() {
-        val nextBoard = elements.toMutableList()
-        repeat(width * width) { nextBoard[it] = Empty }
-        nextBoard[width / 2 - 1, width / 2 - 1] = White
-        nextBoard[width / 2 - 1, width / 2] = Black
-        nextBoard[width / 2, width / 2 - 1] = Black
-        nextBoard[width / 2, width / 2] = White
-        updateBoard(nextBoard)
-    }
-
-    private operator fun List<Piece>.get(x: Int, y: Int): Piece {
-        return this[x + y * 8]
-    }
-
-    private operator fun MutableList<Piece>.set(x: Int, y: Int, cell: Piece) {
-        this[x + y * 8] = cell
+        updateBoard(
+            BoardData(
+                MutableList(boardArea) { Empty }.apply {
+                    this[boardWidth / 2 - 1, boardWidth / 2 - 1] = White
+                    this[boardWidth / 2 - 1, boardWidth / 2] = Black
+                    this[boardWidth / 2, boardWidth / 2 - 1] = Black
+                    this[boardWidth / 2, boardWidth / 2] = White
+                }
+            )
+        )
     }
 }
 
-enum class Piece {
-    Empty,
-    Black,
-    White,
-}
-
-val Piece.opposite: Piece
-    get() = when (this) {
-        Empty -> Empty
-        Black -> White
-        White -> Black
-    }
--- app/src/test/java/com/ry05k2ulv/reversiboard/reversiboard/ReversiBoardTest.kt
+++ app/src/test/java/com/ry05k2ulv/reversiboard/reversiboard/ReversiBoardTest.kt
@@ -12,22 +12,26 @@ class ReversiBoardTest {
 
     private lateinit var subject: ReversiBoard
 
-    private fun randomBoard(width: Int = 8) = MutableList(width * width) {
-        when ((0..2).random()) {
-            0 -> Empty
-            1 -> Black
-            2 -> White
-            else -> throw Exception("randomList: Invalid value")
+    private fun randomBoard(width: Int = 8) = BoardData(
+        MutableList(width * width) {
+            when ((0..2).random()) {
+                0 -> Empty
+                1 -> Black
+                2 -> White
+                else -> throw Exception("randomList: Invalid value")
+            }
         }
-    }
+    )
 
-    private fun initialBoard(width: Int = 8) = MutableList(width * width) { Empty }
-        .apply {
-            this[width / 2 - 1 + (width / 2 - 1) * width] = White
-            this[width / 2 - 1 + (width / 2) * width] = Black
-            this[width / 2 + (width / 2 - 1) * width] = Black
-            this[width / 2 + (width / 2) * width] = White
-        }
+    private fun initialBoard(width: Int = 8) = BoardData(
+        MutableList(width * width) { Empty }
+            .apply {
+                this[width / 2 - 1 + (width / 2 - 1) * width] = White
+                this[width / 2 - 1 + (width / 2) * width] = Black
+                this[width / 2 + (width / 2 - 1) * width] = Black
+                this[width / 2 + (width / 2) * width] = White
+            }
+    )
 
     @BeforeEach
     fun setup() {
@@ -36,29 +40,30 @@ class ReversiBoardTest {
 
     @Test
     fun updateBoard() {
-        val initialList: List<Piece> = initialBoard()
-        subject.updateBoard(initialList)
-        Truth.assertThat(subject.elements).isEqualTo(initialList.toList())
+        val initialBoard = initialBoard()
+        subject.updateBoard(initialBoard)
+        Truth.assertThat(subject.boardData).isEqualTo(initialBoard.copy())
     }
 
     @Test
     fun updateBoard_randomly() {
         repeat(100) {
-            val randomList = randomBoard()
-            subject.updateBoard(randomList)
-            Truth.assertThat(subject.elements).isEqualTo(randomList.toList())
+            val randomBoard = randomBoard()
+            subject.updateBoard(randomBoard)
+            Truth.assertThat(subject.boardData).isEqualTo(randomBoard.copy())
         }
     }
 
-    @Test
-    fun updateBoard_throw_error_when_invalid_length() {
-        arrayOf(0, 7, 9).forEach { width ->
-            val invalidList = randomBoard(width)
-            assertThrows<IllegalArgumentException> {
-                subject.updateBoard(invalidList)
-            }
-        }
-    }
+    // this case is used at adding flexible length board
+//    @Test
+//    fun updateBoard_throw_error_when_invalid_length() {
+//        arrayOf(0, 7, 9).forEach { width ->
+//            val invalidList = randomBoard(width)
+//            assertThrows<IllegalArgumentException> {
+//                subject.updateBoard(invalidList)
+//            }
+//        }
+//    }
 
     @Test
     fun canUndo_return_false_initially() {
@@ -91,11 +96,11 @@ class ReversiBoardTest {
         subject.updateBoard(board2)
         subject.updateBoard(board3)
 
-        Truth.assertThat(subject.elements).isEqualTo(board3.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board3.copy())
         subject.undo()
-        Truth.assertThat(subject.elements).isEqualTo(board2.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board2.copy())
         subject.undo()
-        Truth.assertThat(subject.elements).isEqualTo(board1.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board1.copy())
     }
 
     @Test
@@ -109,109 +114,79 @@ class ReversiBoardTest {
         subject.undo()
         subject.undo()
 
-        Truth.assertThat(subject.elements).isEqualTo(board1.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board1.copy())
         subject.redo()
-        Truth.assertThat(subject.elements).isEqualTo(board2.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board2.copy())
         subject.redo()
-        Truth.assertThat(subject.elements).isEqualTo(board3.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board3.copy())
     }
 
     @Test
     fun drop() {
-        /* Initial
-         *   0 1 2 3 4 5 6 7
-         * 0
-         * 1
-         * 2
-         * 3       o x
-         * 4       x o
-         * 5
-         * 6
-         * 7
-         * */
         val initialBoard = initialBoard()
         subject.updateBoard(initialBoard)
 
-        /* First drop
-         *   0 1 2 3 4 5 6 7
-         * 0
-         * 1
-         * 2
-         * 3       o o o
-         * 4       x o
-         * 5
-         * 6
-         * 7
-         * */
-        val firstBoard = initialBoard.toMutableList()
-            .apply {
-                this[5 + 3 * 8] = White
-                this[4 + 3 * 8] = White
-            }
+        // First drop
+        val firstBoard = BoardData(
+            """
+                - - - - - - - -
+                - - - - - - - -
+                - - - - - - - -
+                - - - o o o - -
+                - - - x o - - -
+                - - - - - - - -
+                - - - - - - - -
+            """.trimIndent().toBoard().first
+        )
         subject.drop(White, 5, 3)
         Truth.assertThat(firstBoard).isNotEqualTo(initialBoard)
-        Truth.assertThat(subject.elements).isEqualTo(firstBoard.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(firstBoard.copy())
 
-        /* Second drop
-         *   0 1 2 3 4 5 6 7
-         * 0
-         * 1
-         * 2       x
-         * 3       x o o
-         * 4       x o
-         * 5
-         * 6
-         * 7
-         * */
-        val secondBoard = firstBoard.toMutableList()
-            .apply {
-                this[3 + 2 * 8] = Black
-                this[3 + 3 * 8] = Black
-            }
+        // Second drop
+        val secondBoard = BoardData(
+            """
+                - - - - - - - -
+                - - - - - - - -
+                - - - x - - - -
+                - - - x o o - -
+                - - - x o - - -
+                - - - - - - - -
+                - - - - - - - -
+            """.trimIndent().toBoard().first
+        )
         subject.drop(Black, 3, 2)
         Truth.assertThat(secondBoard).isNotEqualTo(firstBoard)
-        Truth.assertThat(subject.elements).isEqualTo(secondBoard.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(secondBoard.copy())
 
-        /* Third drop
-         *   0 1 2 3 4 5 6 7
-         * 0
-         * 1
-         * 2       x
-         * 3     o o o o
-         * 4       x o
-         * 5
-         * 6
-         * 7
-         * */
-        val thirdBoard = secondBoard.toMutableList()
-            .apply {
-                this[2 + 3 * 8] = White
-                this[3 + 3 * 8] = White
-            }
+        val thirdBoard = BoardData(
+            """
+                - - - - - - - -
+                - - - - - - - -
+                - - - x - - - -
+                - - o o o o - -
+                - - - x o - - -
+                - - - - - - - -
+                - - - - - - - -
+            """.trimIndent().toBoard().first
+        )
         subject.drop(White, 2, 3)
         Truth.assertThat(thirdBoard).isNotEqualTo(secondBoard)
-        Truth.assertThat(subject.elements).isEqualTo(thirdBoard.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(thirdBoard.copy())
 
-        /* Fourth drop
-         *   0 1 2 3 4 5 6 7
-         * 0
-         * 1
-         * 2       x
-         * 3     o o x o
-         * 4       x x x
-         * 5
-         * 6
-         * 7
-         * */
-        val fourthBoard = thirdBoard.toMutableList()
-            .apply {
-                this[4 + 3 * 8] = Black
-                this[4 + 4 * 8] = Black
-                this[5 + 4 * 8] = Black
-            }
+        val fourthBoard = BoardData(
+            """
+                - - - - - - - -
+                - - - - - - - -
+                - - - x - - - -
+                - - o o x o - -
+                - - - x x x - -
+                - - - - - - - -
+                - - - - - - - -
+            """.trimIndent().toBoard().first
+        )
         subject.drop(Black, 5, 4)
         Truth.assertThat(fourthBoard).isNotEqualTo(thirdBoard)
-        Truth.assertThat(subject.elements).isEqualTo(fourthBoard.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(fourthBoard.copy())
     }
 
     @Test
@@ -219,7 +194,7 @@ class ReversiBoardTest {
         val initialBoard = initialBoard()
         subject.updateBoard(randomBoard())
         subject.reset()
-        Truth.assertThat(subject.elements).isEqualTo(initialBoard.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(initialBoard.copy())
     }
 
 
@@ -230,7 +205,7 @@ class ReversiBoardTest {
         subject.reset()
         Truth.assertThat(subject.canUndo()).isTrue()
         subject.undo()
-        Truth.assertThat(subject.elements).isEqualTo(board.toList())
+        Truth.assertThat(subject.boardData).isEqualTo(board.copy())
         subject.undo()
         Truth.assertThat(subject.canUndo()).isFalse()
     }
